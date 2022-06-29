dbbcc5c243a08d679d75d4f7eb512e5eeff39146
Boris Mitov <boris.m.mitov@gmail.com>
2022-06-29 15:50:14 +0300

    Working with abstractions solutions

--- src/WorkingWithAbstractionLab/rhombusOfStars/RhombusOfStars_01.java
+++ src/WorkingWithAbstractionLab/rhombusOfStars/RhombusOfStars_01.java
@@ -2,7 +2,7 @@ package WorkingWithAbstractionLab.rhombusOfStars;
 
 import java.util.Scanner;
 
-public class Main {
+public class RhombusOfStars_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
