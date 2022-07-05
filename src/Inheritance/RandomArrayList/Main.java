d5b7fb79400d0fd08487c0924f42e779f8d4db6c
Boris Mitov <boris.m.mitov@gmail.com>
2022-07-05 21:36:29 +0300

    Inheritance problem-solving

---
+++ src/Inheritance/RandomArrayList/Main.java
@@ -0,0 +1,14 @@
+package Inheritance.RandomArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        RandomArrayList list = new RandomArrayList();
+        list.add(15);
+        list.add(52);
+        list.add(116);
+        list.add(25);
+        list.add(54);
+
+        System.out.println(list.getRandomElement());
+    }
+}
