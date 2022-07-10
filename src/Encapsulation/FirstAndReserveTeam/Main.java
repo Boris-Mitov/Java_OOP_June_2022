cefc9419c0a6d69cd0ce4a04dfc34be916ff232e
Boris Mitov <boris.m.mitov@gmail.com>
2022-07-10 13:19:10 +0300

    Encapsulation problem-solving

--- src/Encapsulation/FirstAndReserveTeam/Main.java
+++ src/Encapsulation/FirstAndReserveTeam/Main.java
@@ -10,20 +10,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(reader.readLine());
-        List<Person> people = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            String[] input = reader.readLine().split(" ");
-            try {
-                people.add(new Person(input[0], input[1], Integer.parseInt(input[2]), Double.parseDouble(input[3])));
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
-
         Team team = new Team("Ovcha Kupel");
 
-        for (Person player : people) {
-            team.addPlayer(player);
+        for (int i = 0; i < n; i++) {
+            String[] input = reader.readLine().split(" ");
+            String firstName = input[0];
+            String lastName = input[1];
+            int age = Integer.parseInt(input[2]);
+            double salary = Double.parseDouble(input[3]);
+
+            Person personToAdd = new Person(firstName, lastName, age, salary);
+            team.addPlayer(personToAdd);
         }
 
         System.out.printf("First team have %s players%n", team.getFirstTeam().size());
