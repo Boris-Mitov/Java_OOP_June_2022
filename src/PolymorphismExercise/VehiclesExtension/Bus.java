3ef6b55fa4bced54908738b70c984152b7be86b0
Boris Mitov <boris.m.mitov@gmail.com>
2022-07-22 12:15:14 +0300

    Polymorphism problem-solving

---
+++ src/PolymorphismExercise/VehiclesExtension/Bus.java
@@ -0,0 +1,9 @@
+package PolymorphismExercise.VehiclesExtension;
+
+public class Bus extends Vehicle{
+    public final static double ADDITIONAL_FULL_BUS_CONSUMPTION = 1.4;
+
+    public Bus(double fuelQuantity, double fuelConsumptionPerKm, double tankCapacity) {
+        super(fuelQuantity, fuelConsumptionPerKm, tankCapacity);
+    }
+}
